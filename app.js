c08edfca749d3897c10d56f827dd3761a1c17666
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-17 21:53:23 +0300

    feat:add 2 solution server send static files

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
+const http = require("http");
+const fs = require("fs");
+  
+http.createServer((req, res)=>{
+      
+    console.log(`Запрошенный адрес: ${req.url}`);
+    // получаем путь после слеша
+    const filePath = req.url.substring(1);
+    // смотрим, есть ли такой файл
+    fs.access(filePath, fs.constants.R_OK, e => {
+        // если произошла ошибка - отправляем статусный код 404
+        if(e){
+            res.statusCode = 404;
+            res.end("Resourse not found!");
+        }
+        else{
+            fs.createReadStream(filePath).pipe(res);
+        }
+      });
+}).listen(3000, ()=>{
+    console.log("Server started at 3000");
+});
 
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
 
-// node app.js Tom 23
+
+// solution 2
+
+// const http = require("http");
+// const fs = require("fs");
+  
+// http.createServer((req, res)=>{
+//     console.log(`Запрошенный адрес: ${req.url}`);
+//     // получаем путь после слеша
+//     const filePath = req.url.substring(1);
+//     fs.readFile(filePath, (e, data)=>{
+//         if(e){
+//             res.statusCode = 404;
+//             res.end("Resourse not found!");
+//         }   
+//         else{
+//             res.end(data);
+//         }
+//     });
+// }).listen(3000, ()=>{
+//     console.log("Server started at 3000");
+// });
